3dee614a5e95279faa2c7be814fae83cae699f4d
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-19 20:15:11 +0530

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Name : Riddhim Kawdia
+// PRN  : 23070126106
+// Batch: AIML B2
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Cricket Match Begins!");
+
+        MatchManager manager = new MatchManager();
+        manager.startMatch();
+
+        System.out.println("Live from the stadium...");
+    }
+}
